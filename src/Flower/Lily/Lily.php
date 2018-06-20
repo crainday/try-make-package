de6f123e68987fd08a41f1802ebd916187043215
zhaopengju <zhaopengju@zhenrongbao.com>
2018-06-20 14:56:23 +0800

    init && dev package

---
+++ src/Flower/Lily/Lily.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhaopengju
+ * Date: 2018/6/20
+ * Time: 下午2:31
+ */
+
+namespace Flower\Lily;
+
+class Lily
+{
+    public function desc()
+    {
+        echo "this is lily flower";
+    }
+}
---
+++ src/Flower/Rose/Rose.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhaopengju
+ * Date: 2018/6/20
+ * Time: 下午2:31
+ */
+
+namespace Flower\Rose;
+
+class Rose
+{
+    public function desc()
+    {
+        echo "this is rose flower";
+    }
+}
